58f77725f03e1e07d5e80f0815df83bf4a81b00b
Alya <alinataranik03@gmail.com>
2025-08-28 16:34:08 +0300

    add task solution 28.08.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const th = document.querySelectorAll('th');
+
+for (const key of th) {
+  key.addEventListener('click', (events) => {
+    events.preventDefault();
+
+    const index = events.target.cellIndex;
+    const tr = document.querySelectorAll('tbody tr');
+
+    const masiv = [...tr];
+    const tbody = document.querySelector('tbody');
+
+    masiv.sort((a, b) => {
+      if (index === 3) {
+        return (
+          Number(a.cells[index].textContent.replace(/\D/g, '')) -
+          Number(b.cells[index].textContent.replace(/\D/g, ''))
+        );
+      } else {
+        return a.cells[index].textContent.localeCompare(
+          b.cells[index].textContent,
+        );
+      }
+    });
+
+    for (const row of masiv) {
+      tbody.appendChild(row);
+    }
+  });
+}
